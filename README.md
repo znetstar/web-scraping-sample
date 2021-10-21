ef4c130807edc2e42192784f5974955abc8f10c0
ec2-user <ec2-user@ip-10-77-0-248.ec2.internal>
2021-10-21 15:22:10 -0400

    working copy

---
+++ README.md
@@ -0,0 +1,10 @@
+# Web Scraping Sample
+
+This project collects web scraping sample data from presidental approval polls taken from [realclearpolitics](https://realclearpolitics.com).
+
+# Run
+
+1. Copy `.env.sample` to `.env`
+2. Run `npm install`
+3. Start database with `docker-compose up postgres`
+3. Run `npm start`
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 const playwright = require('playwright');
 const path = require('path');
 const fs = require('fs');
+const { Client } = require('pg')
+require('dotenv').config();
+
+const polls = {
+    trump: [
+        'https://www.realclearpolitics.com/epolls/other/president_trump_job_approval-6179.html#polls',
+        2021
+    ],
+    biden: [
+        'https://www.realclearpolitics.com/epolls/other/president-biden-job-approval-7320.html#polls',
+        2021
+    ]
+}
+
 
 async function getApproval(link, endYear = 2021) {
-    let anErr, browser;
+    let anErr, browser, results;
     try {
         // Launch the browser
-        browser = await playwright.chromium.launch();
+        browser = await playwright.chromium.launch({
+            // Actually show the browser (instead of hidden)
+            headless: false
+        });
         const context = await browser.newContext();
         const page = await context.newPage();
 
         // Navigate to the page
-        await page.goto(link);
+        await page.goto(link, {
+            waitUntil: 'domcontentloaded'
+        });
 
         // Inject jQuery into the page.
         // this isn't needed, buy jQuery makes navigating the page
@@ -28,9 +47,10 @@ async function getApproval(link, endYear = 2021) {
         // "ids" (e.g., "id=") are very useful, as as are classes (e.g., "class=")
 
         // ⚠️ This code runs inside the browser! ⚠️
-        await page.evaluate(async (year) => {
+        results = await page.evaluate(async (year) => {
             return (async ($) => {
-                let passOne = false;
+                let passOne = 0;
+                const results = [];
                 // Skip the first two lines
                 for (const row of Array.from($('#polling-data-full tr')).slice(2)) {
                     // Each of the columns
@@ -45,28 +65,30 @@ async function getApproval(link, endYear = 2021) {
                     ] = cols;
 
                     // Now extract the text
-                    const pollster = $(pollsterElement).text().trim();
+                    const pollster = $('a', pollsterElement).first().text().trim();
                     const dateText = $(dateElement).text().trim();
                     const [ start, end ] = dateText.split('-').map(c => new Date(`${c.trim()}/${year}`))
                     // Year goes down if we hit jan again
-                    if (start.getMonth() === 0 && !passOne) {
-                        year--;
-                        passOne = true;
+                    if (start.getMonth() === 0 && ( passOne <= 3 )) {
+                      
+                        passOne++;
                     }
-                    if (start.getMonth() !== 0 && passOne) {
-                        passOne = false;
+                    if (start.getMonth() !== 0 && (passOne > 3)) {
+                        passOne = 0;
+                        year--;
                     }
                     const approve = Number($(approveElement).text().trim());
                     const disapprove = Number($(disapproveElement).text().trim());
 
-
-                    return { 
-                        start, end, approve, disapprove, link, pollster
-                    }
+                    results.push({ 
+                        startDate: start, endDate: end, approve, disapprove, pollster
+                    });
                 }
+                return results;
             })(window.jQuery);
-        }, endYear)
+        }, endYear);
     } 
+    
     // If something goes wrong, we still need to close the browser
     // otherwise we might end up with a ton of idle browsers
     catch (err) {
@@ -76,12 +98,47 @@ async function getApproval(link, endYear = 2021) {
         await browser.close();
         if (anErr)
             throw anErr;
+        return results;
     }
 }
 
 (async () => {
+    // Run the jobs in parallel to save time
+    const allResults = await Promise.all(Object.keys(polls)
+        .map(async (name) => {
+            const results = await getApproval(...polls[name]);
 
+            return results.map(r => {
+                r.name = name;
 
+                return r;
+            })
+        }));
+
+    // Connect to the database
+    const client = new Client(process.env.POSTGRES_URI)
+    await client.connect()
+
+    client.query(`
+        DROP TABLE IF EXISTS records;
+        CREATE TABLE records (
+            startDate date,
+            endDate date,
+            approve int,
+            disapprove int,
+            pollster text,
+            name text
+        );
+    `)
+
+    let lines = [];
+    for (const group of allResults) {
+        for (const record of group) {
+            lines.push(`INSERT INTO records (startDate, endDate, approve, disapprove, pollster, name) VALUES ('${record.startDate.toISOString()}', '${record.endDate.toISOString()}', ${record.approve}, ${record.disapprove}, '${record.pollster}', '${record.name}');`);
+        }
+    }
+
+    return client.query(lines.join("\n"));
 })().catch((err) => {
     console.error(err.stack);
     process.exit(1);
